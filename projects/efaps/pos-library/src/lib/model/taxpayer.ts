f264c4afd33dbad79ee4067980864b6039e95429
Jan Moxter <jan.moxter@moxter.net>
2020-03-27 19:58:49 -0500

    adding properties for taxpayer

--- projects/efaps/pos-library/src/lib/model/taxpayer.ts
+++ projects/efaps/pos-library/src/lib/model/taxpayer.ts
@@ -1,3 +1,21 @@
 export interface Taxpayer {
   id: string;
+  name: string;
+  state: string;
+  homeState: string;
+  ubigeo: string;
+  streetType: string;
+  street: string;
+  zoneCode: string;
+  zoneType: string;
+  streetNumber: string;
+  streetInterior: string;
+  streetBatch: string;
+  apartmentNumber: string;
+  block: string;
+  kilometer: string;
+  department: string;
+  province: string;
+  district: string;
+  address: string;
 }
