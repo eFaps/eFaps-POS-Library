a5441d224c655b37ccbfae6cdf6eaa5264bee279
Jan Moxter <jan.moxter@moxter.net>
2020-04-03 20:41:26 -0500

    Taxpayer search per term

---
+++ projects/efaps/pos-library/src/lib/model/pageable.ts
@@ -0,0 +1,33 @@
+export interface Pageable {
+  sort: Sort;
+  pageNumber: number;
+  pageSize: number;
+  offset: number;
+  paged: boolean;
+  unpaged: boolean;
+}
+
+export interface Sort {
+  sorted: boolean;
+  unsorted: boolean;
+  empty: boolean;
+}
+
+export interface Page<T> {
+  content: T[];
+  pageable: Pageable;
+  totalPages: number;
+  totalElements: number;
+  last: boolean;
+  first: boolean;
+  sort: Sort;
+  numberOfElements: number;
+  size: number;
+  number: number;
+  empty: boolean;
+}
+
+export interface PageRequest {
+  size: number;
+  page: number;
+}
--- projects/efaps/pos-library/src/lib/services/taxpayer.service.ts
+++ projects/efaps/pos-library/src/lib/services/taxpayer.service.ts
@@ -3,6 +3,7 @@ import { HttpClient } from "@angular/common/http";
 import { ConfigService } from "./config.service";
 import { Observable } from "rxjs";
 import { Taxpayer } from "../model";
+import { PageRequest, Page } from "../model/pageable";
 
 @Injectable({
   providedIn: "root",
@@ -14,5 +15,15 @@ export class TaxpayerService {
   public getTaxpayer(id: string): Observable<Taxpayer> {
     const url = `${this.config.baseUrl}/taxpayer/query`;
     return this.http.get<Taxpayer>(url, { params: { id } });
+  }
+
+  public findTaxpayers(
+    term: string,
+    pageable?: PageRequest
+  ): Observable<Page<Taxpayer>> {
+    const url = `${this.config.baseUrl}/taxpayer/query`;
+    const params: any = pageable || {};
+    params.term = term;
+    return this.http.get<Page<Taxpayer>>(url, { params });
   }
 }
