1616c774ded6cc03fb55795195da28c01a05d62a
Jan Moxter <jan.moxter@moxter.net>
2020-05-10 11:38:28 -0500

    Change caching for ImageService

--- projects/efaps/pos-library/src/lib/services/image.service.ts
+++ projects/efaps/pos-library/src/lib/services/image.service.ts
@@ -1,6 +1,5 @@
 import { HttpClient } from "@angular/common/http";
 import { Injectable } from "@angular/core";
-import { Cacheable } from "ngx-cacheable";
 import { Observable, Subscriber } from "rxjs";
 
 import { ConfigService } from "./config.service";
@@ -10,31 +9,37 @@ import { ConfigService } from "./config.service";
   deps: [HttpClient, ConfigService]
 })
 export class ImageService {
+  private cache: Map<string, string> = new Map<string, string>();
+
   constructor(private http: HttpClient, private config: ConfigService) {}
 
-  @Cacheable()
-  public loadImage(_oid: string): Observable<any> {
-    const url = `${this.config.baseUrl}/images/${_oid}`;
-    return new Observable((observer: Subscriber<any>) => {
-      let objectUrl: string = null;
+  public loadImage(oid: string): Observable<string> {
+    if (this.cache.has(oid)) {
+      return this.getFromCache(oid);
+    }
+    return new Observable((subscriber: Subscriber<string>) => {
+      const url = `${this.config.baseUrl}/images/${oid}`;
       this.http.get(url, { responseType: "blob" }).subscribe(m => {
-        objectUrl = URL.createObjectURL(m);
-        observer.next(objectUrl);
+        const objectUrl = URL.createObjectURL(m);
+        this.cache.set(oid, objectUrl);
+        subscriber.next(objectUrl);
       });
-      return () => {
-        if (objectUrl) {
-          URL.revokeObjectURL(objectUrl);
-          objectUrl = null;
-        }
-      };
     });
   }
 
-  @Cacheable()
+  private getFromCache(oid: string): Observable<string> {
+    return new Observable(subscriber => {
+      subscriber.next(this.cache.get(oid));
+      subscriber.complete();
+    });
+  }
+
   getBase64Image(oid: string): Observable<String> {
+    if (this.cache.has(oid)) {
+      return this.getFromCache(oid);
+    }
     const url = `${this.config.baseUrl}/images/${oid}`;
     return new Observable<String>((subscriber: Subscriber<String>) => {
-      let objectUrl: string = null;
       this.http.get(url, { responseType: "blob" }).subscribe(blob => {
         var reader = new FileReader();
         reader.readAsDataURL(blob);
@@ -43,12 +48,13 @@ export class ImageService {
           subscriber.next(base64.toString());
         };
       });
-      return () => {
-        if (objectUrl) {
-          URL.revokeObjectURL(objectUrl);
-          objectUrl = null;
-        }
-      };
     });
+  }
+
+  clear() {
+    this.cache.forEach((value: string, _key: string) => {
+      URL.revokeObjectURL(value);
+    });
+    this.cache.clear()
   }
 }
