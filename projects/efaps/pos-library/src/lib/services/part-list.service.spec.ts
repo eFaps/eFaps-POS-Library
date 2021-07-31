6eedaff236e6501e9233dd96cc8e54ea1db97ea0
Jan Moxter <jan.moxter@moxter.net>
2021-07-30 21:26:22 -0500

    Correct Test

--- projects/efaps/pos-library/src/lib/services/part-list.service.spec.ts
+++ projects/efaps/pos-library/src/lib/services/part-list.service.spec.ts
@@ -1,16 +1,22 @@
-import { TestBed } from '@angular/core/testing';
+import { TestBed } from "@angular/core/testing";
 
-import { PartListService } from './part-list.service';
+import { PartListService } from "./part-list.service";
+import { ProductService } from "./product.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { PosConfigToken } from "./pos-config.token";
 
-describe('PartListService', () => {
+describe("PartListService", () => {
   let service: PartListService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [ProductService, { provide: PosConfigToken, useValue: {} },]
+    });
     service = TestBed.inject(PartListService);
   });
 
-  it('should be created', () => {
+  it("should be created", () => {
     expect(service).toBeTruthy();
   });
 });
