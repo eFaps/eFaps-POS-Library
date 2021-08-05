3b3e609f4eafc811efd562c130cb849dcc06092a
Jan Moxter <jan.moxter@moxter.net>
2021-08-04 21:41:33 -0500

    Adding observable for partlist detection

--- projects/efaps/pos-library/src/lib/services/part-list.service.ts
+++ projects/efaps/pos-library/src/lib/services/part-list.service.ts
@@ -1,13 +1,17 @@
 import { Injectable } from "@angular/core";
 import { ProductService } from "./product.service";
 import { Product, ProductType, Item, ProductRelationType } from "../model";
+import { BehaviorSubject } from "rxjs";
 
 @Injectable({
   providedIn: "root",
   deps: [ProductService]
 })
 export class PartListService {
-  partLists: Product[];
+  private partLists: Product[];
+  private partListComb = [];
+  private partListSource = new BehaviorSubject<Product>(null);
+  detectedPartList = this.partListSource.asObservable();
 
   constructor(private productService: ProductService) {
     this.loadPartLists();
@@ -22,49 +26,57 @@ export class PartListService {
   }
 
   public updateTicket(ticket: Item[]): Item[] {
-    const comp = [];
+    if (this.partLists == undefined) {
+      this.loadPartLists();
+    }
+    if (this.partLists.length > 0) {
+      const ticketComb = [];
+      ticket.forEach(item => {
+        ticketComb.push(item.quantity + "-" + item.product.oid);
+      });
+      const plComb = this.getPartListCombinations();
 
-    ticket.forEach(item => {
-      comp.push(item.quantity + "-" + item.product.oid);
-    });
-    console.log("combinations: " + comp);
-    const comp3 = [];
-    this.partLists.forEach(partList => {
-      const comp2 = [];
-      partList.relations.forEach(relation => {
-        if (ProductRelationType.SALESBOM == relation.type) {
-          comp2.push(relation.quantity + "-" + relation.productOid);
-        }
+      const plHit = plComb.find(pl => {
+        return pl.combinations.every(elem => ticketComb.includes(elem));
       });
-      console.log("comp2: " + comp2);
-      comp3.push({
-        partList,
-        combinations: comp2
-      });
-    });
-    console.log("comp3: " + comp3);
-
-    const plHit = comp3.find(pl => {
-      return pl.combinations.every(elem => comp.includes(elem));
-    });
-    if (plHit) {
-      plHit.partList.relations.forEach(relation => {
-        if (ProductRelationType.SALESBOM == relation.type) {
-          ticket = ticket.filter(item => {
-            return (
-              item.quantity != relation.quantity ||
-              item.product.oid != relation.productOid
-            );
-          });
-        }
-      });
-      ticket.push({
-        product: plHit.partList,
-        quantity: 1,
-        price: plHit.partListcrossPrice,
-        remark: ''
-      });
+      if (plHit) {
+        plHit.partList.relations.forEach(relation => {
+          if (ProductRelationType.SALESBOM == relation.type) {
+            ticket = ticket.filter(item => {
+              return (
+                item.quantity != relation.quantity ||
+                item.product.oid != relation.productOid
+              );
+            });
+          }
+        });
+        ticket.push({
+          product: plHit.partList,
+          quantity: 1,
+          price: plHit.partListcrossPrice,
+          remark: ""
+        });
+        this.partListSource.next(plHit.partList);
+      }
     }
     return ticket;
+  }
+
+  public getPartListCombinations() {
+    if (this.partLists.length > 0 && this.partListComb.length == 0) {
+      this.partLists.forEach(partList => {
+        const comp = [];
+        partList.relations.forEach(relation => {
+          if (ProductRelationType.SALESBOM == relation.type) {
+            comp.push(relation.quantity + "-" + relation.productOid);
+          }
+        });
+        this.partListComb.push({
+          partList,
+          combinations: comp
+        });
+      });
+    }
+    return this.partListComb;
   }
 }
