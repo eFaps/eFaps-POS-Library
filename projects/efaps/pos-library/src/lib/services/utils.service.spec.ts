7d3bc2499f96eb09b4d5bd40afa67d430bf4d884
Jan Moxter <jan.moxter@moxter.net>
2022-08-25 14:13:26 -0500

    Replace dependency with own implementation

--- projects/efaps/pos-library/src/lib/services/utils.service.spec.ts
+++ projects/efaps/pos-library/src/lib/services/utils.service.spec.ts
@@ -12,4 +12,9 @@ describe("UtilsService", () => {
   it("should be created", inject([UtilsService], (service: UtilsService) => {
     expect(service).toBeTruthy();
   }));
+
+  it("should parse correctly", inject([UtilsService], (service: UtilsService) => {
+    expect(service.parse("1,234,657.8901")).not.toBeNaN()
+    expect(service.parse("1,234,657.8901")==1234567.8901).toBeTrue
+  }))
 });
--- projects/efaps/pos-library/src/lib/services/utils.service.ts
+++ projects/efaps/pos-library/src/lib/services/utils.service.ts
@@ -1,19 +1,19 @@
 import { getCurrencySymbol, registerLocaleData } from "@angular/common";
 import localeEsPE from "@angular/common/locales/es-PE";
 import { Injectable } from "@angular/core";
-import parseDecimalNumber from "parse-decimal-number";
 
 @Injectable({
   providedIn: "root",
 })
 export class UtilsService {
+  private numberParser : NumberParser;
   constructor() {
     registerLocaleData(localeEsPE);
+    this.numberParser = new NumberParser("es-PE");
   }
 
   parse(numberStr: string): number {
-    const customSeparators = { thousands: ",", decimal: "." };
-    return parseDecimalNumber(numberStr, customSeparators);
+    return this.numberParser.parse(numberStr);
   }
 
   toString(number: number): string {
@@ -28,5 +28,29 @@ export class UtilsService {
 
   getCurrencySymbol(isoCode: string) {
     return getCurrencySymbol(isoCode, "narrow", "es-PE");
+  }
+}
+
+class NumberParser {
+  private _group: RegExp;
+  private _decimal: RegExp;
+  private _numeral: RegExp;
+  private _index: (d: any) => number;
+  
+  constructor(locale) {
+    const parts = new Intl.NumberFormat(locale).formatToParts(12345.6);
+    const numerals = [...new Intl.NumberFormat(locale, {useGrouping: false}).format(9876543210)].reverse();
+    const index = new Map(numerals.map((d, i) => [d, i]));
+    this._group = new RegExp(`[${parts.find(d => d.type === "group").value}]`, "g");
+    this._decimal = new RegExp(`[${parts.find(d => d.type === "decimal").value}]`);
+    this._numeral = new RegExp(`[${numerals.join("")}]`, "g");
+    this._index = d => index.get(d);
+  }
+
+  parse(string) : number {
+    return (string = string.trim()
+      .replace(this._group, "")
+      .replace(this._decimal, ".")
+      .replace(this._numeral, this._index)) ? +string : NaN;
   }
 }
