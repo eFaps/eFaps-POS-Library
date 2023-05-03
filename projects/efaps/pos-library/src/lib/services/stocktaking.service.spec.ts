3bf30ab52395fc1bae40000dfa4c571e0fbdb3ae
Jan Moxter <jan.moxter@moxter.net>
2023-05-03 00:01:48 -0500

    Correct test

--- projects/efaps/pos-library/src/lib/services/stocktaking.service.spec.ts
+++ projects/efaps/pos-library/src/lib/services/stocktaking.service.spec.ts
@@ -1,12 +1,17 @@
 import { TestBed } from "@angular/core/testing";
 
 import { StocktakingService } from "./stocktaking.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { PosConfigToken } from "./pos-config.token";
 
 describe("StocktakingService", () => {
   let service: StocktakingService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: PosConfigToken, useValue: {} }],
+    });
     service = TestBed.inject(StocktakingService);
   });
 
