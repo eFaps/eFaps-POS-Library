7ddfa2e71cf004976896fde30247bf187d1db547
Jan Moxter <jan.moxter@moxter.net>
2023-12-22 19:01:38 -0500

    Correct test

--- projects/efaps/pos-library/src/lib/services/enquiry.service.spec.ts
+++ projects/efaps/pos-library/src/lib/services/enquiry.service.spec.ts
@@ -1,12 +1,19 @@
 import { TestBed } from "@angular/core/testing";
 
 import { EnquiryService } from "./enquiry.service";
+import { HttpClientTestingModule } from "@angular/common/http/testing";
+import { ConfigService } from "./config.service";
+
+class ConfigServiceStub {}
 
 describe("EnquiryService", () => {
   let service: EnquiryService;
 
   beforeEach(() => {
-    TestBed.configureTestingModule({});
+    TestBed.configureTestingModule({
+      imports: [HttpClientTestingModule],
+      providers: [{ provide: ConfigService, useClass: ConfigServiceStub }],
+    });
     service = TestBed.inject(EnquiryService);
   });
 
